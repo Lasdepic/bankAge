a4072ed5c1bb5c0dd192fea6a4eb26b03cca82f8
Lasdepic <clementjordan34@gmail.com>
2025-07-17 19:15:46 +0200

    Je me demande qui va payer son croissant

--- main.js
+++ main.js
@@ -1,41 +1,60 @@
-//Variable
+// Outils de node.js pour intéragir avec le terminal
 
-let user = "Jordan";
-let age = 30;
+const readline = require('readline'); // https://nodejs.org/api/readline.html#rlquestionquery-options
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Variables
 let compte = 3000;
 let facture = 1200;
 let loisir = 300;
-let retrait = 500;
 
-
-// Fonction calule le solde restant
-
+// Fonction qui calcule le solde restant
 function soldeRestant(compte, facture, loisir) {
-    let solde = compte - facture - loisir;
-    return solde;
+    return compte - facture - loisir;
 }
 
-console.log("Bonjour " + user + ", votre solde est de " + soldeRestant(compte, facture, loisir) + " euros.");
-
-// Fonction de retrait avec conditon pour le retrait d'argent
-
-function retraitArgent(solde, retrait){
-    return solde - retrait; 
+// Fonction de retrait
+function retraitArgent(solde, retrait) {
+    return solde - retrait;
 }
 
-if (age <= 18){
-    console.log("Vous n'avez pas le droit de retirer de l'argent.");
-    return;
-} else {
-    console.log("Vous pouvez retirer de l'argent.");
-}
+// Demande prénom
+rl.question("Comment vous appelez-vous ? ", function(user) {
+    console.log(`Je m'appelle ${user}`);
 
-if (retrait > soldeRestant(compte, facture, loisir)) {
-    console.log("Vous n'avez pas assez d'argent pour retirer " + retrait + " euros.");
-} else {
-    console.log("Opération réussie, vous avez retiré " + retrait + " euros.");
-}
+    // Demande âge
+    rl.question("Quel est votre âge ? ", function(age) {
+        age = parseInt(age); // converti en nombre
 
-console.log("Votre nouveau solde est de " + retraitArgent(soldeRestant(compte, facture, loisir), retrait) + " euros.");
+        // Demande montant à retirer
+        rl.question("Quel montant souhaitez-vous retirer ? ", function(retrait) {
+            retrait = parseInt(retrait); // converti en nombre
 
+            console.log(`Vous avez ${age} ans.`);
 
+            let solde = soldeRestant(compte, facture, loisir);
+            console.log(`Bonjour ${user}, votre solde est de ${solde} euros.`);
+
+            if (age <= 18) {
+                console.log("Vous n'avez pas le droit de retirer de l'argent.");
+            } else {
+                console.log("Vous pouvez retirer de l'argent.");
+
+                if (retrait > solde) {
+                    console.log(`Vous n'avez pas assez d'argent pour retirer ${retrait} euros.`);
+                } else {
+                    console.log(`Opération réussie, vous avez retiré ${retrait} euros.`);
+                    solde = retraitArgent(solde, retrait);
+                }
+
+                console.log(`Votre nouveau solde est de ${solde} euros.`);
+            }
+
+            rl.close();
+        });
+    });
+});
